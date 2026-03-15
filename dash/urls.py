73cc20f2e7a4168917228ba956397cdd13633e90
mediabigscoopstudio <media.bigscoopstudio@gmail.com>
2026-03-15 16:47:40 +0530

    requirements.txt

--- dash/urls.py
+++ dash/urls.py
@@ -7,6 +7,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("",views.index,name='index'),
+    path("login_view",views.login_view,name='login_view'),
+    path("logout_view",views.logout_view,name='logout_view'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
--- dash/views.py
+++ dash/views.py
@@ -1,4 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
 
+
+def superadmin_required(user):
+    return user.is_superuser 
+
+def login_view(request):
+    if request.method == 'POST':  
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+
+    return render(request, 'dash/signin.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('/login_view')
+
+
+@user_passes_test(superadmin_required, login_url=('/login_view'))
 def index(request):
     return render(request,'dash/index.html')
+
