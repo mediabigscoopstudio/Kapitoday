917857fba8d50a2f01008fd76470aacf0e17c29e
mediabigscoopstudio <media.bigscoopstudio@gmail.com>
2026-03-17 10:23:45 +0530

    admin.py update

--- dash/admin.py
+++ dash/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Customers, Category, SubCategory, Product, ProductImage, Variant, Highlight, APlusContent
 
-# Register your models here.
+admin.site.register(Customers)
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(Variant)
+admin.site.register(Highlight)
+admin.site.register(APlusContent)
